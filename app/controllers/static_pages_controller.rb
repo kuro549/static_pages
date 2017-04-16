7b8f6a063de4c93cf7b59a51d3f632eaeb78930b
kuro <kuro@test.com>
2017-04-16 16:52:55 +0000

    update about

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  def home
+    # => app/views/コントローラ名/アクション名.html.erb　がデフォルトで呼び出される
+  end
+
+  def help
+  end
+  
+  def about
+  end
+  
+end
